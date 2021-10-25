c75b2dca808b16c916beae8833bcd42249273fe6
Kwang Hyun <gh9503@gmail.com>
2021-10-25 22:04:44 +0900

    pytest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import jwt
 import bcrypt
+import config
 from flask import Flask, request, jsonify, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
@@ -15,28 +16,6 @@ class CustomJSONEncoder(JSONEncoder):
         if isinstance(obj, set):
             return list(obj)
         return JSONEncoder.default(self, obj)
-
-#Decorators
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        access_token = request.headers.get('Authorization')
-        if access_token is not None:
-            try:
-                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
-            except jwt.InvalidTokenError:
-                payload = None
-
-            if payload is None: return Response(status=401)
-
-            user_id = payload['user_id']
-            g.user_id = user_id
-            g.user = get_user(user_id) if user_id else None
-        else:
-            return Response(status=401)
-
-        return f(*args, **kwargs)
-    return decorated_function
 
 def get_user(user_id):
     user = current_app.database.execute(text("""
@@ -66,7 +45,7 @@ def insert_follow(user_follow):
 
 def insert_unfollow(user_unfollow):
     return current_app.database.execute(text("""
-        DELETE FROM users_follow_list WHERE user_id = :id AND follow_user_id = :unfollow)
+        DELETE FROM users_follow_list WHERE user_id = :id AND follow_user_id = :unfollow
         """), user_unfollow).rowcount
 
 def get_timeline(user_id):
@@ -77,8 +56,31 @@ def get_timeline(user_id):
 
 def get_user_id_and_password(email):
     row = current_app.database.execute(text("""
-        SELECT id, hashed_password FROM users WHERE email = :email"""), {'email':email}).fetchone()
-    return {'id':row['id'],'hashed_password':row['hashed_password']} if row else None
+        SELECT id, hashed_password FROM users WHERE email = :email
+        """), {'email' : email}).fetchone()
+    return {'id' : row['id'], 'hashed_password' : row['hashed_password']} if row else None
+
+#Decorators
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+
+            if payload is None: return Response(status=401)
+
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user(user_id) if user_id else None
+        else:
+            return Response(status=401)
+
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 def create_app(test_config = None):
@@ -103,11 +105,11 @@ def create_app(test_config = None):
     @app.route("/sign-up", methods=['POST'])
     def sign_up():
         new_user = request.json
-        new_user['password']=bcrypt.hashpw(new_user['password'].encode('UTF-8'),bcrypt.gensalt())
+        new_user['password'] = bcrypt.hashpw(new_user['password'].encode('UTF-8'), bcrypt.gensalt())
         new_user_id = insert_user(new_user)
-        new_user_info = get_user(new_user_id)
+        new_user = get_user(new_user_id)
         
-        return jsonify(new_user_info)
+        return jsonify(new_user)
 
     @app.route("/login", methods=['POST'])
     def login():
@@ -119,11 +121,11 @@ def create_app(test_config = None):
         if user_credential and bcrypt.checkpw(password.encode('UTF-8'), user_credential['hashed_password'].encode('UTF-8')):
             user_id = user_credential['id']
             payload = {
-                'user_id':user_id,
-                'exp':datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
+                'user_id' : user_id,
+                'exp' : datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
             }
             token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
-            return jsonify({'access_token':token})
+            return jsonify({'access_token' : token})
         else:
             return '', 401
 
